793478d05959b2b03af9e41b8d194f2db0c11b61
japhet Omorodion <japhet.omorodion@gmail.com>
2019-03-02 10:32:01 +0100

    Model field for product view added

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import ListView
+from . models import product
 
-# Create your views here.
+class ProductList(ListView):
+    queryset = Product.objects.all()
+
+    
